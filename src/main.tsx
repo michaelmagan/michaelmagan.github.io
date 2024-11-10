adc5e9c915f55d0da0022491e4db40766682d514
michaelmagan <michael.magan92@gmail.com>
2024-11-09 20:31:25 -0800

    add motivation page and fix routing :)

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "@/styles/globals.css";
 import App from "./App.tsx";
 import { Toaster } from "@/components/ui/toaster";
-import ElectionBet from "./pages/election"; // Import the new BetTaker component
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/election", // Add a new route for the Bet Taker page
-    element: <ElectionBet />,
-  },
-]);
+import ElectionBet from "./pages/election";
+import MotivationPage from "./pages/motivation";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Toaster />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/election" element={<ElectionBet />} />
+        <Route path="/motivation" element={<MotivationPage />} />
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
   </React.StrictMode>
 );
