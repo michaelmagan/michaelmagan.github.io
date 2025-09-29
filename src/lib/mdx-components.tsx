14eec03c8784eb4232e7bfd17e7ee7fc2d921998
Michael Magan <magan@usehydra.ai>
2025-09-28 20:21:34 -0700

    A few more fixes

--- src/lib/mdx-components.tsx
+++ src/lib/mdx-components.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx";
 import Image from "next/image";
 import type { ImgHTMLAttributes } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type MDXComponents = Record<string, React.ComponentType<any>>;
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
